ef9c10277255e3a137f957270f6dd7abd6f1bb70
csqrb <iliya.11.levchenko@gmail.com>
2022-11-27 02:41:10 +0600

    Flashbangs now has bang property and felenids have increased flash from flashbangs

--- Content.Server/Flash/Components/FlashableComponent.cs
+++ Content.Server/Flash/Components/FlashableComponent.cs
@@ -6,5 +6,12 @@ namespace Content.Server.Flash.Components
     [RegisterComponent, Access(typeof(FlashSystem))]
     public sealed class FlashableComponent : SharedFlashableComponent
     {
+        [DataField("durationMultiplier")]
+        [ViewVariables(VVAccess.ReadWrite)]
+        public float DurationMultiplier { get; set; } = 1;
+
+        [DataField("durationBangMultiplier")]
+        [ViewVariables(VVAccess.ReadWrite)]
+        public float DurationBangMultiplier { get; set; } = 1;
     }
 }
