92b7bb3e930eebe2a9c8412f5cd8151686e008a9
Astronaut <85165800+lAstronautl@users.noreply.github.com>
2024-12-03 13:02:19 +0300

    Merge pull request #20 from Pixeltheaertist/finishing-touches
    finishing up
    # Conflicts:
    #	Content.Shared/Atmos/Atmospherics.cs
    #	Content.Shared/CCVar/CCVars.cs
    #	Resources/Locale/en-US/station-events/events/vent-critters.ftl
    #	Resources/Maps/Misc/terminal.yml
    #	Resources/Maps/RimAavikko/RimAtlas.yml
    #	Resources/Prototypes/Entities/Markers/Spawners/Conditional/timed.yml
    #	Resources/Prototypes/Entities/Structures/Power/Generation/generators.yml
    #	Resources/Prototypes/Entities/Structures/Walls/railing.yml
    #	Resources/Prototypes/Maps/Pools/default.yml

--- Content.Shared/Atmos/Atmospherics.cs
+++ Content.Shared/Atmos/Atmospherics.cs
@@ -1,4 +1,4 @@
-using Robust.Shared.Serialization;
+﻿using Robust.Shared.Serialization;
 // ReSharper disable InconsistentNaming
 
 namespace Content.Shared.Atmos
@@ -77,6 +77,7 @@ namespace Content.Shared.Atmos
 
         public const float OxygenStandard = 0.21f;
         public const float NitrogenStandard = 0.79f;
+        public const float WaterStandard = 7.0f;
 
         public const float OxygenMolesStandard = MolesCellStandard * OxygenStandard;
         public const float NitrogenMolesStandard = MolesCellStandard * NitrogenStandard;
